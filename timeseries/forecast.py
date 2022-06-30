e200c26435b21cbc2097f1212efd86b8e7617f86
vf <79378770+vfcapital@users.noreply.github.com>
2022-06-30 17:00:18 +0200

    finished forecast

--- timeseries/forecast.py
+++ timeseries/forecast.py
@@ -1,7 +1,5 @@
-from scraper import get_coin_returns
 import pandas as pd
-from matplotlib import pyplot
-import warnings
+from matplotlib import pyplot as plt
 from math import sqrt
 from statsmodels.tsa.arima.model import ARIMA
 from sklearn.metrics import mean_squared_error
@@ -20,7 +18,8 @@ def find_least_bitcoin_correl(coin_returns):
 
 def evaluate_arima_model(X, arima_order):
     # prepare training dataset
-    train_size = int(len(X) * 0.66)
+    X = X.values
+    train_size = int(len(X) * 0.8)
     train, test = X[0:train_size], X[train_size:]
     history = [x for x in train]
     # make predictions
@@ -51,3 +50,29 @@ def evaluate_models(dataset, p_values, d_values, q_values):
                 except:
                     continue
     print("Best ARIMA%s RMSE=%.3f" % (best_cfg, best_score))
+
+
+def analyse_final_model(series, arima_order):
+    X = series.values
+    train_size = int(len(X) * 0.8)
+    train, test = X[0:train_size], X[train_size:]
+    history = [x for x in train]
+    # make predictions
+    predictions = list()
+    for t in range(len(test)):
+        model = ARIMA(history, order=arima_order)
+        model_fit = model.fit()
+        yhat = model_fit.forecast()[0]
+        predictions.append(yhat)
+        history.append(test[t])
+    plt.plot(test)
+    plt.plot(predictions, color="red")
+    plt.title(
+        'Coin: "'
+        + str(series.columns[0])
+        + '" - Forecasting ARIMA'
+        + str(arima_order)
+    )
+    plt.grid()
+    plt.show()
+    print(model_fit.summary())
